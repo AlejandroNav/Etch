6eb226d7b747df6bf04d1aa383fb1aa0fb520e4e
Alex-Nav <alejugger@gmail.com>
2021-04-10 00:04:52 -0500

    Complete Etchy

--- javaScript.js
+++ javaScript.js
@@ -2,8 +2,8 @@ const gridContainer = document.querySelector("#grid-container");
 window.addEventListener("load", setDefaultGrid);
 
 function setDefaultGrid() {
-    setGridSize(7);
-    fillGrid(7);
+    setGridSize(9);
+    fillGrid(9);
 }
 
 function setGridSize(size) {
@@ -18,21 +18,27 @@ function fillGrid(size) {
         gridContainer.appendChild(gridElement);
     }
 }
+
 let browns = ['#D29B74', '#C88556', '#B9703C', '#9A5D32', '#7B4B28', '6C4123', '4D2F19']
 
 function changeColor(e) {
+    console.log( "brown colors");
     let rand6 = Math.floor(Math.random() * 6)
     e.target.style.backgroundColor = browns[rand6]
 }
+
 function changeColors(e) {
+    console.log( "rainbow colors");
     const randomR = Math.floor(Math.random() * 256);
     const randomG = Math.floor(Math.random() * 256);
     const randomB = Math.floor(Math.random() * 256);
     e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
+
 const button = document.querySelector("#reset-button");
 button.addEventListener("click", askGrid)
-function askGrid(params) {
+
+function askGrid() {
     let newSize = prompt("Enter new size below 35 and above 1");
     if (newSize !== null) {
         newSize = parseInt(newSize);
@@ -46,24 +52,25 @@ function askGrid(params) {
         }
     }
 }
+
 function clearGrid() {
     const gridArray = Array.from(gridContainer.childNodes);
     gridArray.forEach((element) => {
         gridContainer.removeChild(element);
     });
 }
+
 function cleanGrid() {
     const elements = document.querySelectorAll(".grid-element")
     for (const iterator of elements) {
         iterator.style.backgroundColor = "#e6cab6";
     }
 }
+
 const buttonClear = document.querySelector("#clean-button")
 buttonClear.addEventListener('click', cleanGrid)
-//--------------------------
-//--------------------------
-//--------------------------
 
+//-------------------------- HERE BE BUGS ==================
 
 const buttonColors = document.querySelector("#colors-button")
 buttonColors.addEventListener('click', moveColors)
@@ -72,6 +79,7 @@ function moveColors() {
     const elements = document.querySelectorAll(".grid-element")
     for (const iterator of elements) {
         iterator.addEventListener('mouseover', changeColors)
+        iterator.removeEventListener('mouseover', changeColor)
     }
 }
 
@@ -82,7 +90,8 @@ function moveBrown() {
     console.log('Hello from browns');
     const elements = document.querySelectorAll(".grid-element")
     for (const iterator of elements) {
-        console.log('Hello from browns');
+        console.log('debuggins from from browns');
         iterator.addEventListener('mouseover', changeColor)
+        iterator.removeEventListener('mouseover', changeColors)
     }
 }
